449f32a5613d6b3486b511448c5b8a69ed0cc317
Víctor Beis <vbeisg22@fpcoruna.afundacion.org>
2023-10-01 21:23:52 +0200

    Python ej.6 completado

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -11,13 +11,14 @@ if opcion == "a":
     print("¡RESPUESTA CORRECTA!")
 elif (opcion == "b") | (opcion == "c"):
     print("¡Respuesta errónea!")
-"""
+
+
 
 # EJERCICIO 2
 puntuacion = 0
 
 adivinanzas = {
-    1: "Adivina, adivinanza... que tiene el Iago Pombo panza?",
+    1: "Adivina, adivinanza... que tiene Iago Pombo en la panza?",
     2: "Cuanto tardarán en despedir a Iago Pombo del trabajo?",
     3: "Cuantos hijos tendrá Iago Pombo?",
 }
@@ -71,3 +72,44 @@ if opcion == respuestas[3]:
 elif opcion != respuestas[3]:
     puntuacion = puntuacion - 5
     print("Respuesta incorrecta, -5 puntos.\nPuntuación total: " + str(puntuacion) + "\n")
+"""
+
+# EJERCICIO 6
+import random
+
+puntuacion = 0
+
+adivinanzas = {
+    1: "Adivina, adivinanza... que tiene Iago Pombo en la panza?",
+    2: "Cuanto tardarán en despedir a Iago Pombo del trabajo?",
+    3: "Cuantos hijos tendrá Iago Pombo?",
+}
+
+opciones = {
+    1: "a. Empanadillas\nb. Yogures\nc. El ombligo",
+    2: "a. 2 meses\nb. 2 días\nc. No lo contratan",
+    3: "a. 4 hijos\nb. 4 perros\nc. ∞",
+}
+
+respuestas = {1: "a", 2: "c", 3: "c"}
+
+clavesAdivinanzas = list(adivinanzas.keys())
+listaAdivinanzas = random.sample(clavesAdivinanzas, 2)
+
+for clave in listaAdivinanzas:
+    solucion = respuestas[clave]
+    print("\n" + adivinanzas[clave])
+    print(opciones[clave])
+
+    opcion = input("Elegir: ").strip().lower()
+    while (opcion != "a") & (opcion != "b") & (opcion != "c"):
+        opcion = input("Elige entre a, b o c:").strip().lower()
+
+    if opcion == solucion:
+        print("\n¡RESPUESTA CORRECTA! +10pts :)\n")
+        puntuacion = puntuacion + 10
+    else:
+        print("\n¡RESPUESTA INCORRECTA! -5pts :(\n")
+        puntuacion = puntuacion - 5
+
+    print("Puntos: " + str(puntuacion) + "pts\n")
