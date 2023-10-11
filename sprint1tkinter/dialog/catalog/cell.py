b12f292ce146d64510fe87bce5983410f4c97d53
Víctor Beis <vbeisg22@fpcoruna.afundacion.org>
2023-10-11 09:35:14 +0200

    Tkinter ej.7 completado

--- sprint1tkinter/dialog/catalog/cell.py
+++ sprint1tkinter/dialog/catalog/cell.py
@@ -1,8 +1,10 @@
-from tkinter import Image
-from PIL import ImageTk
+import tkinter as tk
+from PIL import Image, ImageTk
+
 
 class Cell:
-    def __init__(self, title, path):
-        self.title=title
-        self.path=path
-        self.image_tk=ImageTk.PhotoImage(file=self.path)
+    def __init__(self, title, path, desc):
+        self.title = title
+        self.path = path
+        self.image_tk = ImageTk.PhotoImage(file=self.path)
+        self.desc = desc
--- sprint1tkinter/dialog/catalog/window.py
+++ sprint1tkinter/dialog/catalog/window.py
@@ -3,25 +3,26 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from cell import Cell
 from tkinter import messagebox
+from detail_window import detailWindow
 
 class MainWindow:
-    def onButtonClicked(cell):
-        message = "Has hecho click en la celda "+cell.title
-        messagebox.showinfo("Información",message)
+    def onButtonClicked(self, cell):
+        detailWindow(cell)
 
     def __init__(self, root):
-        root.title("MainWindow")
+        root.title("MainWindow")    # Título de la ventana  
 
+        # Lista de objetos
         self.cells = [
-            Cell("Charizard","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/charizard.jpeg"),
-            Cell("Gengar","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/gengar.png"),
-            Cell("Pikachu","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/pikachu.png"),
-            Cell("Snorlax","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/snorlax.jpeg"),
-            Cell("Turtwig","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/turtwig.png"),
+            Cell("Charizard","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/charizard.jpeg","Charizard es un poderoso Pokémon de tipo Fuego/Volador conocido por su imponente apariencia y su aliento de fuego. Es un símbolo de valentía y determinación."),
+            Cell("Gengar","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/gengar.png","Gengar es un astuto Pokémon de tipo Fantasma/Veneno. Siempre acecha en las sombras y disfruta asustando a los incautos. Es un maestro de las artes furtivas."),
+            Cell("Pikachu","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/pikachu.png","Pikachu es la mascota más icónica de Pokémon, conocida por su ternura y su ataque característico, el Rayo. Es un compañero leal y entrañable en las aventuras de entrenadores."),
+            Cell("Snorlax","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/snorlax.jpeg","Snorlax es un masivo Pokémon de tipo Normal que suele dormir y bloquear caminos. Su apetito insaciable y su naturaleza tranquila lo hacen un personaje único."),
+            Cell("Turtwig","/Users/beis/Documentos/di/sprint1tkinter/dialog/catalog/data/unedited/turtwig.png","Turtwig es un Pokémon de tipo Planta. Es conocido por llevar una pequeña planta en su espalda y representa la fuerza de la naturaleza en su crecimiento lento pero constante."),
         ]
 
         for i, cell in enumerate(self.cells):
-            img = Image.open(cell.path)
+            img = Image.open(cell.path) # Abre una sobre la ruta cell.path
             imgResized = img.resize((100,100),Image.Resampling.LANCZOS)
 
             cell.image_tk = ImageTk.PhotoImage(imgResized)
