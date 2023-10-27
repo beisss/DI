b0af0973db16d988b622cf4bd0ded828e181b208
Víctor Beis <vbeisg22@fpcoruna.afundacion.org>
2023-10-27 16:13:05 +0200

    Tkinter 2.2 ej.7 completado

--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -6,14 +6,16 @@ from cell import Cell
 from tkinter import messagebox
 from detail_window import detailWindow
 import cell
+from tkinter import Canvas, Frame, Label, Scrollbar, ttk
 
 class MainWindow:
 
     def __init__(self, root, json_data):
         self.root = root
+        self.root.resizable(True, True)
         self.root.title("MainWindow")    # Título de la ventana  
 
-        cells = [] # Inicializa una lista de celdas vacías
+        self.cells = [] # Inicializa una lista de celdas vacías
 
         x = (self.root.winfo_screenwidth() - self.root.winfo_reqwidth()) / 2
         y = (self.root.winfo_screenheight() - self.root.winfo_reqheight()) / 2
@@ -28,24 +30,33 @@ class MainWindow:
 
             # Crea una instancia de la clase Cell utilizando los datos obtenidos
             cell = Cell(name, image_url, descripcion)
-            cells.append(cell)  # Agrega la instancia recién creada a una lista llamada cells
-
-        # Bucle for que ejecuta su contenido por cada una de las celdas
-        for i, cell in enumerate(cells):
-
-            # Crea una etiqueta que muestra la imagen, el texto y la posiciona
-            label = ttk.Label(
-                root, image=cell.Image_tk, text=cell.title, compound=tk.BOTTOM
-            )
-
-            # Ajusta la posición de manera horizontal
-            label.grid(row=i, column=0)
-
-            # Establece una acción al momento de clicar la etiqueta, ejecuta onButtonClicked
-            label.bind(
-                "<Button-1>",lambda event, cell=cell: detailWindow(cell)
-            )
+            self.cells.append(cell)  # Agrega la instancia recién creada a una lista llamada cells
             
+        # Scrollbar
+        self.canvas = Canvas(self.root)
+        self.scrollbar = Scrollbar (self.root, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame= Frame(self.canvas)
+        #usamos el método bind con Configure para indicar que cada vez que el Frame cambie de
+        #tamaño se actualiza la región de desplazamiento del Canvas.
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+        #indicamos posicion del frame dentro del canvas
+        self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        #refactorizamos codigo para recorrer json 
+        for i, cell in enumerate (self.cells):
+            self.add_item(cell)
+        #posiciones
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.scrollbar.grid(row=0,column=1, sticky="ns")
+
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure (0, weight=1)    
+
         #creamos la barra de menus 
         barra_menus = tk.Menu()
         menu_archivo = tk.Menu(barra_menus, tearoff=False)
@@ -57,12 +68,20 @@ class MainWindow:
                 )
         #añadimos a la barra menus
         barra_menus.add_cascade(menu=menu_archivo, label="Ayuda")
-        root.config(menu=barra_menus)
+        self.root.config(menu=barra_menus)
         #cerramos el bucle
-        root.mainloop()
+        self.root.mainloop()
 
 
-    # Función que ejecuta detailWindow
+    def add_item(self, cell):
+        frame = Frame (self.scrollable_frame)
+        frame.pack(pady=10)
+
+        label = Label (frame, image = cell.Image_tk, text= cell.title, compound= tk.BOTTOM)
+        label.grid(row = 0, column=0)
+
+        label.bind("<Button-1>", lambda event, cell=  cell : detailWindow(cell))
+
     def onButtonClicked(self):
         messagebox.showinfo("Acerca del desarrollador","ama Pokemon")
 
