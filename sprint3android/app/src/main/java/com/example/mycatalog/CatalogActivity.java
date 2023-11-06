07818ed3d1502e408ad9bc19b92e27980ace7107
Víctor Beis <vbeisg22@fpcoruna.afundacion.org>
2023-11-06 16:20:43 +0100

    3.1 Layouts Android ej.2 completado

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,13 +2,28 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
-
+    private Button button1;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        button1 = findViewById(R.id.button1);
+        context = this;
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
