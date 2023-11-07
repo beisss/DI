84f3f4819a23a2895a30e6928031d05783dfe8cf
Víctor Beis <vbeisg22@fpcoruna.afundacion.org>
2023-11-07 09:14:58 +0100

    3.1 Layouts Android ej.4 completado y añadido comentarios

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -14,14 +14,21 @@ public class CatalogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Establecer la vista asociada a esta actividad desde el archivo XML "activity_catalog.xml"
         setContentView(R.layout.activity_catalog);
+
+        // Obtener una referencia al botón con ID "button1" en el diseño
         button1 = findViewById(R.id.button1);
         context = this;
 
+        // Establecer un listener para el botón "button1" que escucha el evento de clic
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Crear un intent para abrir la actividad "DetailActivity"
                 Intent intent = new Intent(context, DetailActivity.class);
+                // Iniciar la actividad "DetailActivity"
                 startActivity(intent);
             }
         });
--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -9,6 +9,8 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Establecer la vista asociada a esta actividad desde el archivo XML "activity_detail.xml"
         setContentView(R.layout.activity_detail);
     }
 }
